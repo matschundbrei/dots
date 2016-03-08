b7f5fd3d82dde49857faa09cf3239c6593a9bc84
Jan Kapellen <jan.kapellen@adtech.de>
2016-03-08 12:55:36 +0100

    all essential dots

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,33 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+ 
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+ 
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , borderWidth		= 1
+	, normalBorderColor	= "grey"
+	, focusedBorderColor	= "red"
+        } `additionalKeys`
+        [ ((mod4Mask, xK_l), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+	, ((mod4Mask, xK_F5), sendMessage Expand)
+        ]
+
